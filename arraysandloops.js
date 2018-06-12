5f0d7900a3e231b5ff07d2acf3f1a3ba4db517fe
Cecille Salazar <cecillersalazar@gmail.com>
2018-06-12 09:58:33 -0700

    Added min function

--- arraysandloops.js
+++ arraysandloops.js
@@ -1,25 +1,30 @@
 'use strict';
-let nums = [3, 65, 12, 9, 81];
+let nums = [765, 65, 12, 9, 81];
 
 function max(numbers) {
   let i = 0;
   let currentMax = 0;
   while (i <= numbers.length) {
     if (numbers[i] > currentMax) {
-      currentMax=numbers[i];
+      currentMax = numbers[i];
     }
     i++;
   }
   return currentMax;
 }
 
-console.log(max(nums));
+//console.log(max(nums));
 
-
-
-/*
 function min(numbers) {
-  // your code here
+  let i = 0;
+  let currentMin = numbers[0];
+  while (i <= numbers.length) {
+    if (currentMin > numbers[i]) {
+      currentMin = numbers[i];
+    }
+    i++;
+  }
+  return currentMin;
 }
 
-*/
+console.log(min(nums));
