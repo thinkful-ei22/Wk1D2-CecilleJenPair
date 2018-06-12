890fd3c871436de601f0a4cee8dca53073b74c6e
Jennifer Colna <jennifer.colna@gmail.com>
2018-06-12 11:44:20 -0500

    max nums

--- arraysandloops.js
+++ arraysandloops.js
@@ -1,12 +1,14 @@
+'use strict';
 let nums = [3, 65, 12, 9, 81];
 
 function max(numbers) {
   let i = 0;
-  let currentMax = numbers[0];
+  let currentMax = 0;
   while (i <= numbers.length) {
     if (numbers[i] > currentMax) {
-      numbers[i] = currentMax;
+      currentMax=numbers[i];
     }
+    i++;
   }
   return currentMax;
 }
